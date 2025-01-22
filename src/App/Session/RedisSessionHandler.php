9d7a8df377e33dc317cd9acba7b41e3d696d0d7b
Tony <57773117@qq.com>
2025-01-22 11:03:22 +0800

    修复RedisSessionHandler connect中的参数类型错误问题

--- src/App/Session/RedisSessionHandler.php
+++ src/App/Session/RedisSessionHandler.php
@@ -15,9 +15,33 @@ declare(strict_types=1);
 
 namespace App\Session;
 
+use Redis;
+use RedisException;
+use RuntimeException;
 use Workerman\Protocols\Http\Session\RedisSessionHandler as RedisHandler;
 
 class RedisSessionHandler extends RedisHandler
 {
-
+    /**
+     * @return void
+     * @throws RedisException
+     */
+    public function connect(): void
+    {
+        $config      = $this->config;
+        $this->redis = new Redis();
+        if (false === $this->redis->connect($config['host'], (int)$config['port'], $config['timeout'])) {
+            throw new RuntimeException("Redis connect {$config['host']}:{$config['port']} fail.");
+        }
+        if (!empty($config['auth'])) {
+            $this->redis->auth($config['auth']);
+        }
+        if (!empty($config['database'])) {
+            $this->redis->select((int)$config['database']);
+        }
+        if (empty($config['prefix'])) {
+            $config['prefix'] = 'redis_session_';
+        }
+        $this->redis->setOption(Redis::OPT_PREFIX, $config['prefix']);
+    }
 }
